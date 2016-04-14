f6150235614b849876ade9ad6472b6070f0f57ee
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2016-04-14 21:14:14 +0200

    Add solution for the minimum scalar product problem

---
+++ code_jam/minimum_product.py
@@ -0,0 +1,11 @@
+def solution(X, Y):
+    return sum([x*y for x, y in zip(sorted(X), sorted(Y, reverse=True))])
+
+if __name__=='__main__':
+    with open('input', 'r') as infile, open('output', 'w') as out:
+        num_testcases = int(next(infile))
+        for i in range(num_testcases):
+            next(infile)
+            X = [int(x) for x in next(infile).split()]
+            Y = [int(y) for y in next(infile).split()]
+            out.write("Case #{0}: {1}\n".format(i+1, solution(X, Y)))
