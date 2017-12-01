e6fe7f9585dcd2630975edc474ffd70bf79cc21d
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-12-01 22:41:22 +0100

    Rename stuff

--- advent_2017/inverse_captcha.py
+++ advent_2017/inverse_captcha.py
@@ -3,6 +3,6 @@ import inputs
 def rotate(l, n):
     return l[-n:] + l[:-n]
 
-input = list(puzzle_1)
+input = list(inputs.captcha)
 print(sum([int(i) for i, j in zip(input, rotate(input, 1)) if i == j]))
 print(sum([int(i) for i, j in zip(input, rotate(input, len(input)//2)) if i == j]))
