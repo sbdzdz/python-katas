5b607ce5b76307bc3799e0d4a76621f10b300cf8
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-12-04 23:28:47 +0100

    I am not proud but it works

--- advent_2017/spiral_memory.py
+++ advent_2017/spiral_memory.py
@@ -1,18 +1,56 @@
 import inputs
-from itertools import count, islice
+from itertools import count, islice, cycle, takewhile, dropwhile
 
-def delta():
-    for index in count():
-        yield [(-1)**(index)] * index
+def build_spiral():
+    left = left_right()
+    up = up_down()
+    while True:
+        for direction in next(left):
+            yield direction
+        for direction in next(up):
+            yield direction 
 
-def distance():
-    x, y = 0, 0
-    for d in delta():
-        for element in d:
-            yield abs(x) + abs(y)
-            x += element
-        for element in d:
-            yield abs(x) + abs(y)
-            y += element
+def left_right():
+    for index, direction in enumerate(cycle('RL')):
+        yield direction*(index+1)
 
-print(list(islice(distance(), inputs.spiral))[-1])
+def up_down():
+    for index, direction in enumerate(cycle('UD')):
+        yield direction*(index+1)
+
+def manhattan(position):
+    return abs(position[0]) + abs(position[1])
+
+def move(position, direction):
+    if direction == 'L':
+        return (position[0]-1, position[1])
+    if direction == 'R':
+        return (position[0]+1, position[1])
+    if direction == 'U':
+        return (position[0], position[1]+1)
+    if direction == 'D':
+        return (position[0], position[1]-1)
+
+def are_neighbors(a, b):
+    return (abs(a[0]-b[0]) < 2) and (abs(a[1]-b[1]) < 2)
+
+def sum_neighbors(spiral, direction):
+    new_element = move(spiral[-1], direction)
+    total = sum(element[2] for element in spiral if are_neighbors(element, new_element))
+    return (new_element[0], new_element[1], total)
+
+def part_one():
+    position = (0, 0)
+    for direction in build_spiral():
+        yield manhattan(position)
+        position = move(position, direction)
+
+def part_two():
+    spiral = [(0, 0, 1)]
+    for direction in build_spiral():
+        new_value = sum_neighbors(spiral, direction)
+        spiral.append(new_value)
+        yield new_value[2]
+
+solution_one = list(islice(part_one(), inputs.spiral))[-1]
+solution_two = next(islice(dropwhile(lambda x: x < inputs.spiral, part_two()), 1))
