22b1d2ea026b0d3678114ae694402a13eddbeb25
Sebastian Dziadzio <sebastian.dziadzio@gmail.com>
2017-12-06 16:51:59 +0100

    Add day 6

---
+++ advent_2017/memory_reallocation.py
@@ -0,0 +1,30 @@
+import inputs
+
+def dist_from_max(index, max_index, length):
+    if index > max_index:
+        return index - max_index
+    else:
+        return length-max_index + index
+
+def update(memory):
+    max_value = max(memory)
+    max_index = memory.index(max_value)
+    memory[max_index] = 0
+    for index, element in enumerate(memory):
+        memory[index] += max_value // len(memory)
+        if dist_from_max(index, max_index, len(memory)) <= max_value % len(memory):
+            memory[index] += 1
+
+def solve(memory):
+    states = dict()
+    while tuple(memory) not in states:
+        states[tuple(memory)] = len(states)
+        update(memory)
+    return len(states), len(states)-states[tuple(memory)]
+
+
+memory_banks = [int(e) for e in inputs.memory.split()]
+part_one, part_two = solve(memory_banks)
+
+print(part_one, part_two)
+
